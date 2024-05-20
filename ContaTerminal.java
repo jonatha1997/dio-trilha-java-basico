43999876872de645dab1209eace38217d7a24571
jonatha1997 <146827809+jonatha1997@users.noreply.github.com>
2024-05-20 08:51:46 -0300

    Add files via upload

---
+++ ContaTerminal.java
@@ -0,0 +1,13 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        //TODO:Por favor, digite o número da conta
+        //TODO:1021
+
+        //TODO:Digite o número da Agência 
+        //TODO:067-8
+
+        //TODO:Saldo R$237.48
+
+        //TODO:Olá MARIO ANDRADE, obrigado por criar uma conta em nosso banco, sua agência é 067-8, conta 1021 e seu saldo 237.48 já está disponível para saque
+    }
+}
